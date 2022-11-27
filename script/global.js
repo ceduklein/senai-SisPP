9578026115bae91ad769939e4481d2c0b50699bf
Carlos Klein <c.edudepaula@yahoo.com.br>
2022-11-27 18:53:37 -0300

    1ª Versão finalizada

--- script/global.js
+++ script/global.js
@@ -1,4 +1,4 @@
-function openModal(x) {
+const openModal = (x) => {
   let modal = document.getElementById(x);
   if (typeof modal == 'undefined' || modal === null)
     return;
@@ -7,11 +7,39 @@ function openModal(x) {
   document.body.style.overflow = 'hidden';
 }
 
-function closeModal(x) {
+const closeModal = (x) => {
   let modal = document.getElementById(x);
   if (typeof modal == 'undefined' || modal === null)
     return;
   
   modal.style.display = 'none';
   document.body.style.overflow = 'auto';
+}
+
+const estados = ['--', 'AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO',
+  'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS',
+  'RO', 'RR', 'SC', 'SE', 'SP', 'TO'];
+
+const regioes = ['--', 'Centro-Oeste', 'Nordeste', 'Norte', 'Sudeste', 'Sul'];
+
+const loadUF = (uf) => {
+  let select = document.getElementById(uf);
+  
+  estados.forEach(e => {
+    let option = document.createElement('option');
+    option.text = e;
+    option.value = e;
+    select.add(option);
+  });
+}
+
+function loadRegiao(regiao) {
+  let select = document.getElementById(regiao);
+
+  regioes.forEach(r => {
+    let option = document.createElement('option');
+    option.text = r;
+    option.value = r;
+    select.add(option);
+  })
 }
